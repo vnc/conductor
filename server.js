780020828fb1f1fa1e01348f929595fc5c9101bf
Chris Castle <crcastle@gmail.com>
2011-02-17 22:43:06 -0800

    began creating router setup. next step is to create functions that make the EC2 API calls.

--- server.js
+++ server.js
@@ -1,32 +1,56 @@
 var http = require('http'),
-	router = require('choreographer').router();
+	router = require('choreographer').router(),
+	ec2 = require('aws-lib').createEC2Client(key, secretKey);
 
 
 // ec2 requests require the following URL format
-// create: http://0.0.0.0/ec2/<ACTION>?imageId=&instanceType&az=&kernelId=&ramDiskId=&securityGroups=&
+// create: http://0.0.0.0/ec2/<ACTION>?imageId=&instanceType&az=&kernelId=&ramDiskId=&secGroups=&userData=
 // where the correct domain is used and <ACTION> is replaced with
 // one of create, start, stop, or terminate
 // TODO: allow comma separated list of instance IDs also
-router.get('/ec2/create', function(req, res) {
+router.get('/ec2/*', function(req, res, action) {
 	var u = url.parse(req.url, true);
-	var queryString = u.query;
+	var q = u.query;
+	
+	var r = this;
+	switch(action) {
+		case 'create':
+			createInstances(q, function(httpCode, message) {
+				res.writeHead(httpCode, { 'Content-Type': 'text/plain' } );
+				res.end(message);
+			});
+			break;
+		case 'start':
+			startInstances(q, function(httpCode, message) {
+				res.writeHead(httpCode, { 'Content-Type': 'text/plain' } );
+				res.end(message);
+			});
+			break;
+		case 'stop':
+			stopInstances(q, function(httpCode, message) {
+				res.writeHead(httpCode, { 'Content-Type': 'text/plain' } );
+				res.end(message);
+			});
+			break;
+		case 'terminate':
+			terminateInstances(q, function(httpCode, message) {
+				res.writeHead(httpCode, { 'Content-Type': 'text/plain' } );
+				res.end(message);
+			});
+			break;
+		default:
+			r.notFound(notFound);
+	}
+	
+	
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end("The instance a-89eb98be was succesfully started.");
 })
-.get('/ec2/start', function(req, res) { // start: http://0.0.0.0/ec2/start?instanceId=
-	
-})
-.get('/ec2/stop', function(req, res) { // stop: http://0.0.0.0/ec2/stop?instanceId=
-	
-})
-.get('/ec2/terminate', function(req, res) { // stop: http://0.0.0.0/ec2/terminate?instanceId=
-	
-})
-.notFound(function(req, res)
-{
+.notFound(notFound);
+
+var notFound = function(req, res) {
 	res.writeHead(404, {'Content-Type': 'text/plain'});
-	res.end('404: Man.... look what you did! You done gone broke this thing.  I\'m tellin. \n' +
-		'No seriously, you attempt has been logged, and the proper authorities have been notified.');
-});
+	res.end('404: Whatever you\'re trying to do isn\'t going to work here.');
+};
 
 http.createServer(router).listen(8080);
